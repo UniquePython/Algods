98151b4e4fd89498e4774e6a99c7d33c11a53aa5
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2024-08-10 18:44:07 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='algods',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[],
     entry_points={'console_scripts': [],},
@@ -17,5 +17,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.12',
+    python_requires='>=3.8',
 )
